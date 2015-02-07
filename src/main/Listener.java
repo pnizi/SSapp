e049e529e1ad9825642471fe54d51de0060edab9
pnizi <pnizi@users.noreply.github.com>
2015-02-06 21:37:54 -0600

    Update Listener.java

--- src/main/Listener.java
+++ src/main/Listener.java
@@ -1,65 +1,54 @@
 package main;
 
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
-public class Listener implements KeyListener
+import com.melloware.jintellitype.HotkeyListener;
+import com.melloware.jintellitype.JIntellitype;
+
+
+
+public class Listener implements HotkeyListener
 {
+	static Boolean KeyListener = true;
 	
-	public Listener()
+	public void Listener()
 	{
+		JIntellitype.getInstance().registerHotKey(0, 0, KeyEvent.VK_F9); 
+		JIntellitype.getInstance().registerHotKey(1, 0, KeyEvent.VK_F10); 
+		JIntellitype.getInstance().registerHotKey(2, 0, KeyEvent.VK_F11); 
+		JIntellitype.getInstance().addHotKeyListener(this);
 		
 	}
 
-	@Override
-	public void keyPressed(KeyEvent e)
+	public void cleanup()
 	{
-		if(e.getKeyCode() == KeyEvent.VK_F9)
+		JIntellitype.getInstance().unregisterHotKey(0);
+		JIntellitype.getInstance().unregisterHotKey(1);
+		JIntellitype.getInstance().unregisterHotKey(2);
+		JIntellitype.getInstance().cleanUp();
+		//System.exit(0);
+	}
+	
+	public void onHotKey(int key)
+	{
+		Screenshot TakeScreenShot = new Screenshot();
+		if(key == 0)
 		{
-			System.out.println("f9 pressed");
+			TakeScreenShot.fullscreenSS();
+			System.out.println("Full Screenshot taken");
+		    
 		}
-		if(e.getKeyCode() == KeyEvent.VK_F10)
+		if(key == 1)
 		{
-			System.out.println("f10 pressed");
+			TakeScreenShot.windowSS();
+			System.out.println("Current Window screenshot taken");
 		}
-		if(e.getKeyCode() == KeyEvent.VK_F11)
+		if(key == 2)
 		{
+			TakeScreenShot.areaSS();
 			System.out.println("f11 pressed");
 		}
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e)
-	{
-		if(e.getKeyCode() == KeyEvent.VK_F9)
-		{
-			System.out.println("f9 released");
-		}	
-		if(e.getKeyCode() == KeyEvent.VK_F10)
-		{
-			System.out.println("f10 released");
-		}
-		if(e.getKeyCode() == KeyEvent.VK_F11)
-		{
-			System.out.println("f11 released");
-		}
 		
 	}
-
-	@Override
-	public void keyTyped(KeyEvent e)
-	{
-		if(e.getKeyCode() == KeyEvent.VK_F9)
-		{
-			System.out.println("f9 released");
-		}	
-		if(e.getKeyCode() == KeyEvent.VK_F10)
-		{
-			System.out.println("f10 released");
-		}
-		if(e.getKeyCode() == KeyEvent.VK_F11)
-		{
-			System.out.println("f11 released");
-		}
-	}
+	
 }
