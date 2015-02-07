73364fe59405e880b3b319db40a0e731af579591
pnizi <pnizi@users.noreply.github.com>
2015-02-06 21:38:12 -0600

    Update Run.java

--- src/main/Run.java
+++ src/main/Run.java
@@ -2,9 +2,29 @@ package main;
 
 public class Run
 {
-	
-public static void main(String [] args)
-{
-	
-}
+	public static TrayManager TrayMGR = new TrayManager();
+	public static void main(String [] args) throws Exception
+	{
+	     TrayMGR.createAndShowGUI();
+		 Listener key = new Listener();  
+		 Listener.KeyListener = true;
+	     key.Listener();
+	     
+	     Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+	    	    public void run() {
+	    	     key.cleanup();
+	    	    }
+	    	}));
+	     while (Listener.KeyListener) 
+	     {  
+	         try 
+	         {  
+	             Thread.sleep(5000);  
+	         } 
+	         catch (Exception ex) 
+	         {  
+	             break;  
+	         }  
+	     }    
+	}
 }
